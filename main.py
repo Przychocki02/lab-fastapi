536e95d6d9ee17f2d2e94f103656e7d2bf1d7a25
Faberrr1 <jakubkowalczyk923@gmail.com>
2023-05-24 08:46:38 +0200

    Trzeci commit

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class StudentCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
+
 
 app = FastAPI()
 
+students = []
+@app.post("/students/")
+async def create_item(item: StudentCreateSchema):
+    students.append(item)
+    return item
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/students/{id}")
+async def root(id: int):
+    return {"student": students[id]}
+async def read_item(id: int):
+    return {"item_id": id}
