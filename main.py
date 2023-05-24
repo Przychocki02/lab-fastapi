b94df4fbb6aa5108e66602c7e7378815439b5318
Faberrr1 <jakubkowalczyk923@gmail.com>
2023-05-24 09:05:49 +0200

    Czwarty commit

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ class StudentCreateSchema(BaseModel):
     first_name: str
     last_name: str
 
+class StudentUpdateSchema(BaseModel):
+    name: str
+    surname: str
 
 app = FastAPI()
 
@@ -20,3 +23,18 @@ async def root(id: int):
     return {"student": students[id]}
 async def read_item(id: int):
     return {"item_id": id}
+
+@app.put("/student/{id}")
+def update_student(id: int, updated_data: StudentUpdateSchema):
+    if id not in students:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    # Aktualizacja imienia i nazwiska studenta
+    students[id]["name"] = updated_data.name
+    students[id]["surname"] = updated_data.surname
+
+    return students[id]    
+    
+    
+    
+    
